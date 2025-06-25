0c337c721b8d5e427b6cee71981c29023ff28e87
Javier1908 <ja.vasquezd@duocuc.cl>
2025-06-25 10:44:46 -0400

    Primera carga prueba  Parcial

---
+++ prueba 2.py
@@ -0,0 +1,55 @@
+def comprar_entrada(compradores):
+    while True:
+        nombre = input("Ingrese el nombre del comprador: ")
+        if not nombre:
+            print("El nombre del comprador no puede estar vacío.")
+            continue
+        if nombre in compradores:
+            print("El nombre del comprador ya existe. Por favor, use un nombre diferente.")
+            continue
+        break
+
+    while True:
+        tipo_entrada = input("Ingrese el tipo de entrada (G para General, V para VIP): ")
+        if tipo_entrada not in ["G", "V"]:
+            print("Tipo de entrada inválido. Por favor, ingrese 'G' o 'V'.")
+            continue
+        break
+
+    while True:
+        codigo_confirmacion = input("Ingrese el código de confirmación (mínimo 6 caracteres, al menos 1 mayúscula, 1 número, sin espacios): ")
+        if len(codigo_confirmacion) < 6:
+            print("El código de confirmación debe tener al menos 6 caracteres.")
+            continue
+        if not any(char.isupper() for char in codigo_confirmacion):
+            print("El código de confirmación debe contener al menos una letra mayúscula.")
+            continue
+        if not any(char.isdigit() for char in codigo_confirmacion):
+            print("El código de confirmación debe contener al menos un número.")
+            continue
+        if " " in codigo_confirmacion:
+            print("El código de confirmación no puede contener espacios en blanco.")
+            continue
+        break
+    compradores[nombre] = {"tipo_entrada": tipo_entrada, "codigo_confirmacion": codigo_confirmacion}
+    print("¡Entrada registrada exitosamente!")
+
+def consultar_comprador(compradores):
+    nombre = input("Ingrese el nombre del comprador a consultar: ")
+    if nombre in compradores:
+        datos = compradores[nombre]
+        print(f"\nDatos del comprador '{nombre}':")
+        print(f"  Tipo de Entrada: {datos["tipo_entrada"]}")
+        print(f"  Código de Confirmación: {datos["codigo_confirmacion"]}")
+    else:
+        print("El comprador no se encuentra.")
+
+
+def cancelar_compra(compradores):
+    nombre = input("Ingrese el nombre del comprador cuya compra desea cancelar: ")
+    if nombre in compradores:
+        del compradores[nombre]
+        print(f"La compra para '{nombre}' ha sido cancelada exitosamente.")
+    else:
+        print("El comprador no se encuentra, no se puede cancelar la compra.")
+
